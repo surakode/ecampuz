2c0b7b0238a5315014cdfa628520394373f5ad33
Sura <suratno@kencana-alam.com>
2022-06-28 12:20:20 +0700

    revisi nomer 4

--- 4.php
+++ 4.php
@@ -1,13 +1,20 @@
 <!DOCTYPE html>
 <html>
 
-<!-- MAAF KEHABISAN WAKTU -->
-
 <body>
     <?php
     for ($i = 1; $i <= 50; $i++) {
 
-        echo " $i <br>"; 
+        if (fmod($i, 3)  == 0 && fmod($i, 5) == 0) {
+            echo "FooBar" . "<br>"; 
+        }
+        else if(fmod($i, 3)  == 0) {
+            echo "Foo" . "<br>"; 
+        }
+        else if(fmod($i, 5)  == 0) {
+            echo "Bar" . "<br>"; 
+        }
+        
     }
     ?>
 
